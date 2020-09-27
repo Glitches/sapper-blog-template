f5046e27d15918cfbfa3e95f83fa94f0850d3e70
Andrea <andrea.ceccarelli@facile.it>
2020-09-27 19:48:26 +0200

    first commit

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -22,15 +22,33 @@
 </svelte:head>
 
 <div class="container">
-  <h1>About</h1>
+  <h1>Andrea Ceccarelli</h1>
   <figure>
-    <img src='rsz_florian-klauer-489-unsplash.jpg' alt='Image of a vintage typewriter.'>
-    <figcaption>Photo by <a href="https://unsplash.com/@florianklauer" target="_blank">Florian Klauer</a> on Unsplash</figcaption>
+    <img
+      src="rsz_florian-klauer-489-unsplash.jpg"
+      alt="Image of a vintage typewriter." />
+    <figcaption>
+      Photo by
+      <a href="https://unsplash.com/@florianklauer" target="_blank">Florian
+        Klauer</a>
+      on Unsplash
+    </figcaption>
   </figure>
-  <p>Text placeholder via <a href="https://jeffsum.com/" target="_blank">Jeffsum</a>.</p>
-  <p>So you two dig up, dig up dinosaurs? What do they got in there? King Kong? My dad once told me, laugh and the world laughs with you, Cry, and I'll give you something to cry about you little bastard! Life finds a way. God creates dinosaurs. God destroys dinosaurs. God creates Man. Man destroys God. Man creates Dinosaurs.</p>
-  <p>You really think you can fly that thing? You know what? It is beets. I've crashed into a beet truck. Forget the fat lady! You're obsessed with the fat lady! Drive us out of here! Is this my espresso machine? Wh-what is-h-how did you get my espresso machine?</p>
-  <p>Hey, you know how I'm, like, always trying to save the planet? Here's my chance. Hey, take a look at the earthlings. Goodbye! I was part of something special. Just my luck, no ice. You're a very talented young man, with your own clever thoughts and ideas. Do you need a manager?</p>
-  <p>Jaguar shark! So tell me - does it really exist? This thing comes fully loaded. AM/FM radio, reclining bucket seats, and... power windows. Yes, Yes, without the oops! You're a very talented young man, with your own clever thoughts and ideas. Do you need a manager?</p>
-  <p>Yes, Yes, without the oops! Do you have any idea how long it takes those cups to decompose. They're using our own satellites against us. And the clock is ticking. Do you have any idea how long it takes those cups to decompose. My dad once told me, laugh and the world laughs with you, Cry, and I'll give you something to cry about you little bastard!</p>
+  <p>Benvenuto!</p>
+  <p>
+    Mi chiamo Andrea Ceccarelli e sono uno sviluppatore Front-end e talvolta
+    Back-end, vivo e lavoro a Milano
+  </p>
+  <p>
+    Fino a pochi anni fa la mia vita era completamente diversa ma intorno al
+    2017 ho deciso di cambiare direzione e diventare sviluppatore. Questo mi ha
+    portato a realizzare un progetto di vita e realizzare un sogno.
+  </p>
+  <p>
+    Questo sito è basato sul template
+    <a href="https://github.com/Charca/sapper-blog-template">
+      Charca/sapper-blog-template
+    </a>
+    che uso per studiare Svelte e Sapper
+  </p>
 </div>
