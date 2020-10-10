5fd79c2518a037c0f2f1bd19e4e94e72107fb581
Andrea <andrea.ceccarelli@facile.it>
2020-10-10 16:55:07 +0200

    feature: change names

--- src/components/Bio.svelte
+++ src/components/Bio.svelte
@@ -19,11 +19,14 @@
 </style>
 
 <div>
-  <img src="profile-pic.png" alt="Maxi Ferreira">
+  <img src="profile-pic.png" alt="Maxi Ferreira" />
   <p>
-    Hi, I'm <strong>Maxi</strong>. I'm a software engineer and data scientist from Córdoba, Argentina.
-    You can follow me on <a href="https://www.twitter.com/Charca" target="_blank">Twitter</a>,
-    see some of my work on <a href="https://www.github.com/Charca" target="_blank">GitHub</a>,
-    or read more about me on <a href="https://www.maxiferreira.com" target="_blank">my website</a>.
+    Hi, I'm
+    <strong>Andrea</strong>. I'm a web developer from Lucca, Italy. You can
+    follow me on
+    <a
+      href="https://www.twitter.com/NethanKossovsky"
+      target="_blank">Twitter</a>, see some of my work on
+    <a href="https://www.github.com/Glitches" target="_blank">GitHub</a>.
   </p>
 </div>
