a99debaa5d3780bc6b5543a94269fe6b7f7e61a1
Andrea Ceccarelli <gltch2003@gmail.com>
2025-04-20 17:08:47 +0200

    Feat: add simple about me page

---
+++ src/routes/about/+page.svelte
@@ -0,0 +1,34 @@
+<a class="text-blue-500" href="/">Home</a>
+<h1 class="mx-auto p-4 text-3xl font-bold underline">Chi sono</h1>
+<article class="max-w-lg overflow-hidden">
+  <p class="p-4">
+    Mi chiamo Andrea Ceccarelli, sono un software engineer e questo è il mio
+    angolo sul web, dove racconto di codice, corse, cinema e tutto quello che mi
+    appassiona.
+  </p>
+  <p class="p-4">
+    Per più di dieci anni ho lavorato nel mondo dell'automotive, occupandomi di
+    revisioni auto e altri mestieri pratici. Ma la curiosità è una brutta bestia
+    — e a un certo punto ho capito che volevo cambiare strada. Così ho iniziato
+    a studiare programmazione da autodidatta, passando ore e ore tra tutorial,
+    documentazioni e notti insonni davanti a uno schermo. Poi ho fatto le
+    valigie e sono andato a Barcellona, dove ho frequentato una scuola di
+    coding. Un'esperienza che mi ha aperto la porta al mondo dello sviluppo
+    software. Tornato in Italia, sono approdato in Facile.it, dove ho avuto la
+    fortuna di lavorare su progetti diversi e crescere come sviluppatore fino a
+    diventare un full-stack software engineer.
+  </p>
+  <p class="p-4">
+    Oggi lavoro con TypeScript, Node.js, Symfony, NestJS e altre tecnologie che
+    mi permettono di costruire cose utili (e a volte anche belle). Ma non vivo
+    solo di codice: corro regolarmente (e mi sto preparando per una mezza
+    maratona), bevo birra artigianale con la stessa serietà con cui scrivo test
+    automatizzati, adoro il cinema e leggo un sacco di fantascienza.
+  </p>
+  <p class="p-4">
+    Questo blog nasce per condividere quello che imparo, raccontare i miei
+    progetti, e ogni tanto scrivere due righe su ciò che mi appassiona. Se sei
+    arrivato fin qui, grazie del tempo — magari ci beviamo una birra virtuale
+    insieme tra un post e l’altro 🍻
+  </p>
+</article>
